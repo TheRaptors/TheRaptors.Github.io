51792fcdbb6c05928867f3100c3184a3950eb505
TheRaptors <Mcsys@hotmail.com>
2017-09-20 15:04:27 +0800

    2017-09-20 15:04:27

--- PythonSpider/main.sh
+++ PythonSpider/main.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 if [ $(($(date "+%H") % 12)) == 0 ]; then
-    python ./XunLeiBiZhi.py ./XunLeiBiZhi/$(date "+%Y-%m-%d_%H:%M:%S")
+    python ./XunLeiBiZhi.py ./XunLeiBiZhi/$(date "+%Y-%m-%d_%H:%M:%S").txt
 fi
 
 if [ $(($(date "+%H") % 6)) == 0 ]; then
-    python ./NeiHanFuLiShe_iOS.py ./NeiHanFuLiShe_iOS/$(date "+%Y-%m-%d_%H:%M:%S")
+    python ./NeiHanFuLiShe_iOS.py ./NeiHanFuLiShe_iOS/$(date "+%Y-%m-%d_%H:%M:%S").txt
     cd ../ && sh ./GitHubForLinux.sh
 fi
