8a7d660e5779a801233a20939b15b85e1530dbe9
TheRaptors <Mcsys@hotmail.com>
2017-10-10 18:13:14 +0800

    2017-10-10 18:13:14

--- PythonSpider/BaiSiBuDeJie.py
+++ PythonSpider/BaiSiBuDeJie.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 #  导入模块
@@ -20,7 +20,7 @@ def Get_Index(URL, Page):
             print('发布：' + Item[1])
             print('时间：' + Item[3])
             print('标题：' + Item[6])
-            print('链接：' + Item[8].replace('"', '').replace('data-original=', '').replace('data-mp4=', ''))
+            print('链接：' + Item[8].replace('"', '').replace('data-original=', '').replace('data-mp4=', '').replace('data-pic=', ''))
             Count += 1
     except urllib.request.URLError as Error:
         if hasattr(Error, 'code'):
@@ -134,112 +134,32 @@ def Get_Hot(URL, Page):
             print(Error.reason)
 
 # 最大显示50页
+Min_Page = 1
 Max_Page = 51
 
-# 首页
-for Page in range(1, Max_Page):
-    URL = 'http://www.budejie.com/'
-    if Page == 1:
-        URL = URL
-    else:
-        URL = URL + str(Page)
-    Get_Index(URL = URL, Page = Page)
+# 基础域名
+Basic_URL = 'http://www.budejie.com/'
 
-# 首页-穿越
-for Page in range(1, Max_Page):
-    URL = 'http://www.budejie.com/old/'
-    if Page == 1:
-        URL = URL
-    else:
-        URL = URL + str(Page)
-    Get_Index(URL = URL, Page = Page)
+# 扩展信息
+List={'Type1': '首页', 'Domain1': '', 'Func1': Get_Index,
+      'Type2': '首页-穿越', 'Domain2': 'old', 'Func2': Get_Index,
+      'Type3': '视频', 'Domain3': 'video', 'Func3': Get_Video,
+      'Type4': '视频-穿越', 'Domain4': 'old-video', 'Func4': Get_Video,
+      'Type5': '图片', 'Domain5': 'pic', 'Func5': Get_Pic,
+      'Type6': '图片-穿越', 'Domain6': 'old-pic', 'Func6': Get_Pic,
+      'Type7': '段子', 'Domain7': 'text', 'Func7': Get_Text,
+      'Type8': '段子-穿越', 'Domain8': 'old-text', 'Func8': Get_Text,
+      'Type9': '声音', 'Domain9': 'audio', 'Func9': Get_Audio,
+      'Type10': '声音-穿越', 'Domain10': 'old-audio', 'Func10': Get_Audio,
+      'Type11': '排行', 'Domain11': 'hot', 'Func11': Get_Hot,
+      'Type12': '美女', 'Domain12': 'tag/117', 'Func12': Get_Hot}
 
-# 视频
-for Page in range(1, Max_Page):
-    URL = 'http://www.budejie.com/video/'
-    if Page == 1:
-        URL = URL
-    else:
-        URL = URL + str(Page)
-    Get_Video(URL = URL, Page = Page)
-
-# 视频-穿越
-for Page in range(1, Max_Page):
-    URL = 'http://www.budejie.com/old-video/'
-    if Page == 1:
-        URL = URL
-    else:
-        URL = URL + str(Page)
-    Get_Video(URL = URL, Page = Page)
-
-# 图片
-for Page in range(1, Max_Page):
-    URL = 'http://www.budejie.com/pic/'
-    if Page == 1:
-        URL = URL
-    else:
-        URL = URL + str(Page)
-    Get_Pic(URL = URL, Page = Page)
-
-# 图片-穿越
-for Page in range(1, Max_Page):
-    URL = 'http://www.budejie.com/old-pic/'
-    if Page == 1:
-        URL = URL
-    else:
-        URL = URL + str(Page)
-    Get_Pic(URL = URL, Page = Page)
-
-# 段子
-for Page in range(1, Max_Page):
-    URL = 'http://www.budejie.com/text/'
-    if Page == 1:
-        URL = URL
-    else:
-        URL = URL + str(Page)
-    Get_Text(URL = URL, Page = Page)
-
-# 段子-穿越
-for Page in range(1, Max_Page):
-    URL = 'http://www.budejie.com/old-text/'
-    if Page == 1:
-        URL = URL
-    else:
-        URL = URL + str(Page)
-    Get_Text(URL = URL, Page = Page)
-
-# 声音
-for Page in range(1, Max_Page):
-    URL = 'http://www.budejie.com/audio/'
-    if Page == 1:
-        URL = URL
-    else:
-        URL = URL + str(Page)
-    Get_Audio(URL = URL, Page = Page)
-
-# 声音-穿越
-for Page in range(1, Max_Page):
-    URL = 'http://www.budejie.com/old-audio/'
-    if Page == 1:
-        URL = URL
-    else:
-        URL = URL + str(Page)
-    Get_Audio(URL = URL, Page = Page)
-    
-# 排行
-for Page in range(1, Max_Page):
-    URL = 'http://www.budejie.com/hot/'
-    if Page == 1:
-        URL = URL
-    else:
-        URL = URL + str(Page)
-    Get_Hot(URL = URL, Page = Page)
-
-# 美女
-for Page in range(1, Max_Page):
-    URL = 'http://www.budejie.com/tag/117/'
-    if Page == 1:
-        URL = URL
-    else:
-        URL = URL + str(Page)
-    Get_Hot(URL = URL, Page = Page)
+for Page in range(Min_Page, Max_Page):
+    for i in range(1, 13):
+        # print(List['Type' + str(i)])
+        URL = Basic_URL + List['Domain' + str(i)]
+        if Page == 1:
+            URL = URL
+        else:
+            URL = URL + '/' + str(Page)
+        List['Func' + str(i)](URL = URL, Page = Page)
